a7c62b19f55ee7eb16088b7322e0e73eb0d2bca7
stfwn <s.wijnja@me.com>
2020-01-26 18:34:26 +0100

    Repurpose file to plot most important graphs

--- logs/analyze.py
+++ logs/analyze.py
@@ -6,24 +6,47 @@ from helpers import *
 
 def main():
     all_runs = get_runs()
-    lenet_kn = filter_runs(all_runs, model='lenet', init='kaiming-normal')
-    lenet_ku = filter_runs(all_runs, model='lenet', init='kaiming-uniform')
-    lenet_xn = filter_runs(all_runs, model='lenet', init='xavier-normal')
-    lenet_xu = filter_runs(all_runs, model='lenet', init='xavier-uniform')
+    lenet_runs = filter_runs(all_runs, model='lenet')
+
+    kn = filter_runs(lenet_runs, init='kaiming-normal')
+    ku = filter_runs(lenet_runs, init='kaiming-uniform')
+    xn = filter_runs(lenet_runs, init='xavier-normal')
+    xu = filter_runs(lenet_runs, init='xavier-uniform')
+    normal = filter_runs(lenet_runs, init='normal')
+    uniform = filter_runs(lenet_runs, init='uniform')
+    xu_half = filter_runs(lenet_runs, init='xavier-uniform-half')
+    xu_double = filter_runs(lenet_runs, init='xavier-uniform-double')
+    xn_half = filter_runs(lenet_runs, init='xavier-normal-half')
+    xn_double = filter_runs(lenet_runs, init='xavier-normal-double')
 
     # Set var to examine and gather run data in matrix
     var = 'test_acc'
-    data = [np.array([run[var] for run in lenet_kn]),
-            np.array([run[var] for run in lenet_ku]),
-            np.array([run[var] for run in lenet_xn]),
-            np.array([run[var] for run in lenet_xu])]
+    data = [np.array([run[var] for run in kn]),
+            np.array([run[var] for run in ku]),
+            np.array([run[var] for run in xn]),
+            np.array([run[var] for run in xu]),
+            np.array([run[var] for run in normal]),
+            np.array([run[var] for run in uniform]),
+            np.array([run[var] for run in xu_double]),
+            np.array([run[var] for run in xu_half]),
+            np.array([run[var] for run in xn_double]),
+            np.array([run[var] for run in xn_half]),
+            ]
 
     # Prepare line data to plot
     lines = []
-    lines.append(('lenet_kn', np.mean(data[0], axis=0), np.std(data[0], axis=0)))
-    lines.append(('lenet_ku', np.mean(data[1], axis=0), np.std(data[1], axis=0)))
-    lines.append(('lenet_xn', np.mean(data[2], axis=0), np.std(data[2], axis=0)))
-    lines.append(('lenet_xu', np.mean(data[3], axis=0), np.std(data[3], axis=0)))
+    #lines.append(('kn', np.mean(data[0], axis=0), np.std(data[0], axis=0)))
+    #lines.append(('ku', np.mean(data[1], axis=0), np.std(data[1], axis=0)))
+
+    lines.append(('xu', np.mean(data[3], axis=0), np.std(data[3], axis=0)))
+    lines.append(('xu_double', np.mean(data[6], axis=0), np.std(data[6], axis=0)))
+    lines.append(('xu_half', np.mean(data[7], axis=0), np.std(data[7], axis=0)))
+
+    #lines.append(('normal', np.mean(data[4], axis=0), np.std(data[4], axis=0)))
+    #lines.append(('uniform', np.mean(data[5], axis=0), np.std(data[5], axis=0)))
+    lines.append(('xn', np.mean(data[2], axis=0), np.std(data[2], axis=0)))
+    lines.append(('xn_double', np.mean(data[8], axis=0), np.std(data[8], axis=0)))
+    lines.append(('xn_half', np.mean(data[9], axis=0), np.std(data[9], axis=0)))
 
     pruning_rate = 0.2
     pruning_interval = 5
@@ -31,48 +54,24 @@ def main():
     total_epochs = 100
     X = range(int(total_epochs/pruning_interval))
 
-    fig, ax = plt.subplots(1, 1, constrained_layout=True)
+    fig, axs = plt.subplots(2, 1, constrained_layout=True)
+    xticklabels = 100 * (1-pruning_rate) ** np.arange(0,20)
+    for ax in axs.flatten():
+        ax.grid(True, linestyle=':')
+        ax.set_xticks(range(20))
+        ax.set_xticklabels(np.round(xticklabels, 1))
+        ax.set_xlabel('% weights remaining')
+        ax.set_ylabel(var)
 
-    for line in lines:
+    for i, line in enumerate(lines):
         y = line[1][start::pruning_interval]
         yerr = line[2][start::pruning_interval]
-        ax.errorbar(X, y, yerr=yerr, label=line[0])
+        axs.flatten()[i//3].errorbar(X, y, yerr=(yerr/2), label=line[0])
 
-    ax.set_xticks(range(20))
+    for ax in axs.flatten():
+        ax.legend()
+
     # Set other stuff
-    xticklabels = 100 * (1-pruning_rate) ** np.arange(0,20)
-    ax.set_xticklabels(np.round(xticklabels, 1))
-    ax.set_xlabel('% weights remaining')
-    ax.set_ylabel(var)
-    ax.legend()
-    plt.show()
-    # TODO: why are some errorbars > 100?
-
-
-
-
-
-
-
-    
-
-def plot_1():
-    var = 'test_acc'
-    a = np.mean(np.array([run[var] for run in lenet_kn]), axis=0)
-    b = np.mean(np.array([run[var] for run in lenet_ku]), axis=0)
-    c = np.mean(np.array([run[var] for run in lenet_xn]), axis=0)
-    d = np.mean(np.array([run[var] for run in lenet_xu]), axis=0)
-    fig, axs = plt.subplots(1, 1, constrained_layout=True)
-    axs.set_xlim(0,100)
-    axs.set_ylim(50,100)
-    axs.plot(a, label='lenet_kn')
-    axs.plot(b, label='lenet_ku')
-    axs.plot(c, label='lenet_xn')
-    axs.plot(d, label='lenet_xu')
-
-    axs.set_xlabel('epoch')
-    axs.set_ylabel('test_acc')
-    axs.legend()
     plt.show()
 
 
